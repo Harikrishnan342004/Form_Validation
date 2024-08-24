6d5ffb24b8c4ca20151b3c4a13fc20bb4437ef99
Hari <harikrishnan53828@gmail.com>
2024-08-24 16:27:13 +0530

    country code

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ function validateForm()
             alert("Name must be filled out");
             return false;
         }
-    }
+
+        let org_phone = document.forms["myForm"]["phone"].value;
+        let alt_phone = document.forms["myForm"]["falt_phone"].value;
+
+        if( org_phone == alt_phone)
+        {
+            alert("Give different phone number");
+            return false
+             
+        }
+
+
+        let org_email = document.forms["myForm"]["f_email"].value;
+        let alt_email = document.forms["myForm"]["falt_email"].value;
+
+        if( org_email == alt_email)
+        {
+            alert("Give different mail id");
+            return false
+             
+        }
+
+    } 
